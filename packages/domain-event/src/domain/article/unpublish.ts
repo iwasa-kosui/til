a07ceaf97e77c74a1289aef636748f3f5eafd597
ksakamoto-kkhs <k.sakamoto@kakehashi.life>
2025-05-29 14:38:08 +0900

    unpublished.ts を実装

--- packages/domain-event/src/domain/article/unpublish.ts
+++ packages/domain-event/src/domain/article/unpublish.ts
@@ -1,21 +1,36 @@
-import type { InReviewArticle, PublishedArticle, Article } from "./article.js";
+import { err, ok, type Result } from 'neverthrow';
+import { assertNever } from "../../util/assertNever.js";
+import type { InReviewArticle, PublishedArticle, Article, ArticleEvent } from "./article.js";
 import { ArticleStatus } from "./articleStatus.js";
+import { AlreadyInReviewError, StillDraftError } from "./error.js";
+import { DomainEvent } from '../domainEvent.js';
 
+/**
+ * 記事公開取消イベント
+ */
+export type ArticleUnpublished = ArticleEvent<'ArticleUnpublished', {}, InReviewArticle>;
+
+export const ArticleUnpublished = {
+    from: (article: PublishedArticle): ArticleUnpublished =>
+        DomainEvent.from('ArticleUnpublished', {}, {
+            ...article,
+            status: ArticleStatus.IN_REVIEW,
+        }),
+} as const;
 
 
 /**
  * 記事の公開を取り消します。
  */
-export const unpublish = (article: Article): InReviewArticle => {
+export const unpublish = (article: Article): Result<ArticleUnpublished, StillDraftError | AlreadyInReviewError> => {
     switch (article.status) {
         case ArticleStatus.DRAFT:
-            throw new Error('Draft article cannot be unpublished');
+            return err(StillDraftError.from(article));
         case ArticleStatus.IN_REVIEW:
-            throw new Error('Already in review');
+            return err(AlreadyInReviewError.from(article));
         case ArticleStatus.PUBLISHED:
-            return {
-                ...article,
-                status: ArticleStatus.IN_REVIEW,
-            }
+            return ok(ArticleUnpublished.from(article));
+        default:
+            return assertNever(article);
     }
 }
